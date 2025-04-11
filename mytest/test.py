29e888f425e1d919e79438ef24a698c88e9c7abd
zzhbrr <zzhbrr@gmail.com>
2025-04-11 15:03:54 +0800

    add acc_length results

--- mytest/test.py
+++ mytest/test.py
@@ -12,7 +12,7 @@ from torch.utils.data.dataloader import DataLoader
 from tqdm import tqdm
 import numpy as np
 
-draft_token_number = 10
+draft_token_number = 30
 
 model = EaModel.from_pretrained(
     base_model_path="/data1/zzh/huggingface/hub/models--mistralai--Mixtral-8x7B-Instruct-v0.1/snapshots/41bd4c9e7e4fb318ca40e721131d4933966c2cc1",
@@ -27,16 +27,28 @@ model.eval()
 
 tokenizer = model.get_tokenizer()
 # tokenizer = AutoTokenizer.from_pretrained("/data1/zzh/huggingface/hub/models--mistralai--Mixtral-8x7B-Instruct-v0.1/snapshots/41bd4c9e7e4fb318ca40e721131d4933966c2cc1", use_fast=False)
-seq_len = 128
 tokenizer.pad_token = tokenizer.eos_token
-dataset = load_dataset("cnn_dailymail", "1.0.0", split="test[0:1000]")
-num_eval_steps = 200
+seq_len = 128
+# dataset = load_dataset("cnn_dailymail", "1.0.0", split="test[0:1000]")
+dataset = load_dataset("liyucheng/sharegpt-500", split="train")
+num_eval_steps = 25
 for i in tqdm(range(num_eval_steps)):
 # for i in range(num_eval_steps):
-    sequence = dataset[i]["article"]
-    inputs_id = tokenizer(sequence, return_tensors='pt',max_length=seq_len,padding=True,truncation=True)
-    input_ids = inputs_id['input_ids'][..., :128]
-    attention_mask = inputs_id['attention_mask'][..., :128]
+    # apply template
+    # sequence = dataset[i]["article"]
+    sequence = dataset[i]["chat"][0][1]
+    sequence = sequence[:128]
+    conv = get_conversation_template("mixtral")
+    conv.append_message(conv.roles[0], sequence)
+    conv.append_message(conv.roles[1], None)
+    prompt = conv.get_prompt()
+    input_ids=tokenizer([prompt]).input_ids
+    input_ids = torch.as_tensor(input_ids).cuda()
+    # chat_input = [{"role": "user", "content": sequence}]
+    # prompt = tokenizer.apply_chat_template(chat_input, tokenize=False)
+    # inputs_id = tokenizer(prompt, return_tensors='pt',max_length=seq_len,padding=True,truncation=True)
+    # input_ids = inputs_id['input_ids'][..., :128]
+    # attention_mask = inputs_id['attention_mask'][..., :128]
     output_ids=model.eagenerate(input_ids,temperature=0.5,max_new_tokens=128)
 
 acc_length_list = model.get_accept_length_logger()
@@ -45,6 +57,9 @@ print(np.mean(acc_length_list))
 savefile = "/home/zzh/codes/SpecMoE/EagleTest/EAGLE/mytest/log.txt"
 with open(savefile, "a") as f:
     f.write(f"draft token: {draft_token_number}, mean accept length: {np.mean(acc_length_list)}\n")
+savefile2 = f"/home/zzh/codes/SpecMoE/EagleTest/EAGLE/mytest/log_accpe_length_{draft_token_number}.txt"
+with open(savefile2, "a") as f:
+    f.write(f"{acc_length_list}\n")
 
 
 # your_message="Hello, do you know where is Nanjing University Xianlin Campus?"
